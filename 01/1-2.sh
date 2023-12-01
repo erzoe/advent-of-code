984478bd7c812cf737e4f14911c6790fb04002aa
erik <debian>
2023-12-01 12:23:58 +0100

    note why 1-2.sh does not work
    it was added in commit 80a71dc952f683ceba280e2b9aac3fc8054a310e

--- 01/1-2.sh
+++ 01/1-2.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This does not give the desired result because it interprets jcb82eightwond as 88 instead of 82.
+# I think this was not described well enough in the task and it was not covered in the example.
+
 cp input puzzle
 sed -Ei 's/(one|two|three|four|five|six|seven|eight|nine)/<\1>/g; s/<one>/1/g; s/<two>/2/g; s/<three>/3/g; s/<four>/4/g; s/<five>/5/g; s/<six>/6/g; s/<seven>/7/g; s/<eight>/8/g; s/<nine>/9/g;' puzzle
 paste -d '' <(sed -E 's/[^0-9]*([0-9]).*/\1/' puzzle) <(sed -E 's/.*([0-9])[^0-9]*$/\1/' puzzle) | paste -sd '+' | bc
