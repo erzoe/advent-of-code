480048e52c41558a12ef454b35fcd4d685f5faf8
erik <debian>
2023-12-05 08:48:04 +0100

    5-1: passes example

---
+++ 05/rs/src/main.rs
@@ -0,0 +1,76 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+struct MapEntry {
+    dst: u32,
+    src: u32,
+    len: u32,
+}
+
+struct Map {
+    entries: Vec<MapEntry>,
+}
+
+fn main() {
+    let file = File::open("../../exp").unwrap_or_else(|_| panic!("file not found"));
+    let reader = BufReader::new(file);
+    let mut iterator = reader.lines().map(|ln| ln.unwrap());
+    let start_seeds = parse_start_seeds(&iterator.next().expect("file is empty"));
+    let mut maps = Vec::<Map>::new();
+
+    for ln in iterator {
+        if ln.is_empty() {
+            // ignore empty lines
+        } else if ln.ends_with(':') {
+            // start new map
+            maps.push(Map::new());
+        } else {
+            maps.last_mut().expect("map entry before first map start").entries.push(MapEntry::parse(&ln));
+        }
+    }
+
+    let mut min_location = u32::MAX;
+    for seed in start_seeds {
+        let mut result = seed;
+        for map in &maps {
+            result = map.lookup(result);
+        }
+        if result < min_location {
+            min_location = result;
+        }
+    }
+    println!("min_location: {min_location}");
+}
+
+fn parse_start_seeds(ln: &str) -> Vec<u32> {
+    let (_, seeds) = ln.split_once(": ").expect("failed to split first line");
+    seeds.split(' ').map(|s| s.parse::<u32>().expect("failed to parse start seed")).collect()
+}
+
+impl MapEntry {
+    fn parse(ln: &str) -> Self {
+        let mut s = ln.split(' ');
+        let dst = s.next().expect("missing destination range start").parse::<u32>().expect("failed to parse destination range start");
+        let src = s.next().expect("missing source range start").parse::<u32>().expect("failed to parse source range start");
+        let len = s.next().expect("missing range length").parse::<u32>().expect("failed to parse range length");
+        if let Some(_value) = s.next() {
+            panic!("unexpected value after range length in line {ln}");
+        }
+        Self {dst, src, len}
+    }
+}
+
+impl Map {
+    fn new() -> Self {
+        Self { entries: Vec::new() }
+    }
+    fn lookup(&self, val: u32) -> u32 {
+        for entry in &self.entries {
+            if entry.src <= val && val < entry.src + entry.len {
+                return val - entry.src + entry.dst;
+            }
+        }
+        val
+    }
+}
