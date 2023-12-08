4f56b22ab6bfafd8654c5ec2738aae40365b085a
erik <debian>
2023-12-08 09:16:39 +0100

    8-1: correct solution

--- 08/rs/src/main.rs
+++ 08/rs/src/main.rs
@@ -32,13 +32,13 @@ struct Directions {
 
 
 fn main() {
-    let file = BufReader::new(File::open("../../exp_6_steps").expect("input file does not exist"));
+    let file = BufReader::new(File::open("../../input").expect("input file does not exist"));
     let mut lines = file.lines().map(|ln| ln.unwrap());
     let mut directions = Directions::parse(&lines.next().unwrap());
     let nodes = Nodes::parse(lines);
 
     let mut node = nodes.get_start();
-    let mut count: u8 = 0;
+    let mut count: u32 = 0;
     while !node.is_goal() {
         let direction = directions.next();
         println!("{node} {direction:?}");
