eb8251848f9f5412fbc8d610908b447b33127334
erik <debian>
2023-12-12 08:10:52 +0100

    8-2 idea for new approach

--- 08/rs_8-2/src/main.rs
+++ 08/rs_8-2/src/main.rs
@@ -3,6 +3,10 @@ extern crate macros;
 
 definitions!();
 
+//Warning: don't try to run this, it takes way too long and a u32 is not even big enough.
+// I guess I could try to loop over every node individually until I find a repetition in (index_direction, node)
+// and then only check the steps where the longest period has it's goals.
+
 fn main() {
     let mut nodes = START_NODES;
     let mut step: u32 = 0;
